7af13a963dde03f559051d78161ca1cc167f9849
Nicole Cardoso <00001106178968sp@aluno.educacao.sp.gov.br>
2025-06-18 20:37:56 -0300

    oi

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,8 +1,9 @@
 function about () {
     return (
         <>
-          <p> "Gosto de desenvolver coisas usando Python, MySQL e Android Studio. Curto aprender na prática e sempre tento deixar os projetos funcionando direitinho. Se você curte programação também, bora trocar ideia!"</p>
+          <p>"Gosto de desenvolver coisas usando Python, MySQL e Android Studio. Curto aprender na prática e sempre tento deixar os projetos funcionando direitinho. Se você curte programação também, bora trocar ideia!"</p>
           <p>Minhas Habilidades: Eu sei fazer codigos para fazer aplicativos, e tenho uma boa comunicação com as pessoas.</p>
+          <p>"Projetos: Combustex, Meus colegas e eu criamos um posto de gasolina usando python e Html. "</p>
         </>
     )
 }
