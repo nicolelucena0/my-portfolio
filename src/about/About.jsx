8f8d04d7e296080aeeb5bc2771dfb762a9eec4a4
Nicole Cardoso <00001106178968sp@aluno.educacao.sp.gov.br>
2025-06-18 16:01:46 -0300

    Atualização

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -2,6 +2,7 @@ function about () {
     return (
         <>
           <p>Eu sou desenvolvedora FullStack</p>
+          <p>Não tenho nem um curso, mais tenho várias habilidaes ex: cozinho cuido da minha irmã e da minha biza avó sou da midia na igreja que eu congrego e entre outra coisas, mais gosto muito aprender programação. </p>
         </>
     )
 }
